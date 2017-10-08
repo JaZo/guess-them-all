e1d37e64afb6fbc80bc4073450456a3aa7ecd79f
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2017-10-08 16:11:08 +0200

    Moved route definition to separate file

--- src/router/index.js
+++ src/router/index.js
@@ -1,83 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/pages/Home';
-import Teams from '@/pages/Teams';
-import Team from '@/pages/Team';
-import Game from '@/pages/Game';
-import GameIndex from '@/pages/game/Index';
-import GameReady from '@/pages/game/Ready';
-import GameGame from '@/pages/game/Game';
-import GameTimeOver from '@/pages/game/TimeOver';
-import GameResults from '@/pages/game/Results';
-import GameGameOVer from '@/pages/game/GameOver';
+import routes from './routes'
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-            meta: {
-                allowBack: false
-            }
-        },
-        {
-            path: '/teams',
-            name: 'teams',
-            component: Teams,
-            meta: {
-                allowBack: true
-            }
-        },
-        {
-            path: '/teams/:id',
-            name: 'team',
-            component: Team,
-            meta: {
-                allowBack: true
-            }
-        },
-        {
-            path: '/game',
-            name: 'game',
-            component: Game,
-            meta: {
-                allowBack: false
-            },
-            children: [
-                {
-                    path: 'index',
-                    name: 'game-index',
-                    component: GameIndex
-                },
-                {
-                    path: 'ready',
-                    name: 'game-ready',
-                    component: GameReady
-                },
-                {
-                    path: 'game',
-                    name: 'game-game',
-                    component: GameGame
-                },
-                {
-                    path: 'time-over',
-                    name: 'game-time-over',
-                    component: GameTimeOver
-                },
-                {
-                    path: 'results',
-                    name: 'game-results',
-                    component: GameResults
-                },
-                {
-                    path: 'game-over',
-                    name: 'game-game-over',
-                    component: GameGameOVer
-                }
-            ]
-        }
-    ]
+    routes
 });
---
+++ src/router/routes.js
@@ -0,0 +1,77 @@
+import Home from '@/pages/Home';
+import Teams from '@/pages/Teams';
+import Team from '@/pages/Team';
+import Game from '@/pages/Game';
+import GameIndex from '@/pages/game/Index';
+import GameReady from '@/pages/game/Ready';
+import GameGame from '@/pages/game/Game';
+import GameTimeOver from '@/pages/game/TimeOver';
+import GameResults from '@/pages/game/Results';
+import GameGameOVer from '@/pages/game/GameOver';
+
+export default [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            allowBack: false
+        }
+    },
+    {
+        path: '/teams',
+        name: 'teams',
+        component: Teams,
+        meta: {
+            allowBack: true
+        }
+    },
+    {
+        path: '/teams/:id',
+        name: 'team',
+        component: Team,
+        meta: {
+            allowBack: true
+        }
+    },
+    {
+        path: '/game',
+        name: 'game',
+        component: Game,
+        meta: {
+            allowBack: false
+        },
+        children: [
+            {
+                path: 'index',
+                name: 'game-index',
+                component: GameIndex
+            },
+            {
+                path: 'ready',
+                name: 'game-ready',
+                component: GameReady
+            },
+            {
+                path: 'game',
+                name: 'game-game',
+                component: GameGame
+            },
+            {
+                path: 'time-over',
+                name: 'game-time-over',
+                component: GameTimeOver
+            },
+            {
+                path: 'results',
+                name: 'game-results',
+                component: GameResults
+            },
+            {
+                path: 'game-over',
+                name: 'game-game-over',
+                component: GameGameOVer
+            }
+        ]
+    }
+];
