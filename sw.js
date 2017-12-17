b656337cc86dab8633d80e96f2411e0bdacaa16c
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2017-12-17 21:01:33 +0100

    Automatic build 2017-12-17 21:01:27

---
+++ sw.js
@@ -0,0 +1,46 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "index.html",
+    "revision": "1032b58ee483b36100cde5710e6202ee"
+  },
+  {
+    "url": "static/css/app.b9c9fa896bb2ec819e75f1cdee28e480.css"
+  },
+  {
+    "url": "static/js/app.3fd30b71255596e86c05.js"
+  },
+  {
+    "url": "static/js/manifest.6a99a06364de6e806ba4.js"
+  },
+  {
+    "url": "static/js/vendor.e31ae3fbe8cf1d2042a2.js"
+  }
+];
+
+const workboxSW = new self.WorkboxSW({
+  "cacheId": "guess-them-all",
+  "skipWaiting": true,
+  "clientsClaim": true,
+  "directoryIndex": "index.html"
+});
+workboxSW.precache(fileManifest);
+workboxSW.router.registerNavigationRoute("/");workboxSW.router.registerRoute(/fonts.(gstatic|googleapis).com\/(.*)/, workboxSW.strategies.staleWhileRevalidate({}), 'GET');
