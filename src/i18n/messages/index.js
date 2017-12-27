42207c8d0abdb94e6f70b9d2797d4185d871d7af
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2017-12-27 15:51:19 +0100

    Added hot reloading for translations

--- src/i18n/messages/index.js
+++ src/i18n/messages/index.js
@@ -1,3 +1,4 @@
+import i18n from '../index';
 import en from './en';
 import nl from './nl';
 
@@ -5,3 +6,11 @@ export default {
     en,
     nl
 };
+
+// Hot updates
+if (module.hot) {
+    module.hot.accept(['./en', './nl'], () => {
+        i18n.setLocaleMessage('en', en);
+        i18n.setLocaleMessage('nl', nl);
+    });
+}
