4f8cdeca386123c81190692d655410e2bddfe4cf
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2018-01-07 16:18:43 +0100

    Split entities into multiple sets

---
+++ src/data/entities/index.js
@@ -0,0 +1,9 @@
+import google from './nl/google.json';
+import susanne from './nl/susanne.json';
+
+export default {
+    nl: {
+        google,
+        susanne
+    }
+};
--- src/services/entities.js
+++ src/services/entities.js
@@ -1,17 +1,35 @@
-import entities from '../data/entities/nl.json'
+import entities from '../data/entities';
 
 export default class Entities {
     /**
      * @param {Number} amount
-     * @param {Array} excludedEntities
+     * @param {Array}  excludedEntities
+     *
      * @returns {Array}
      */
     static get(amount, excludedEntities = []) {
-        return entities.entities
+        return this.combined()
             .filter((entity) => {
                 return excludedEntities.indexOf(entity) < 0;
             }).sort(() => {
                 return 0.5 - Math.random();
             }).slice(0, amount);
+    }
+
+    /**
+     * @returns {Array}
+     */
+    static combined() {
+        let combined = new Set();
+
+        entities.nl.google.entities.forEach((entity) => {
+            combined.add(entity);
+        });
+
+        entities.nl.susanne.entities.forEach((entity) => {
+            combined.add(entity);
+        });
+
+        return [...combined];
     }
 }
