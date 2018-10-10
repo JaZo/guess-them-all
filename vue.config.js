a532e290bac6952b71d2e25c6806a2326ca2ccdc
Jasper Zonneveld <jasper.zonneveld@gmail.com>
2018-10-10 21:28:37 +0200

    Use vue-cli-plugin-gh-pages for deployments

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 let pkg = require('./package.json');
 
+const currentDateTime = (function currentDateTime () {
+    const date = new Date();
+
+    let hour = date.getHours();
+    hour = (hour < 10 ? '0' : '') + hour;
+
+    let min = date.getMinutes();
+    min = (min < 10 ? '0' : '') + min;
+
+    let sec = date.getSeconds();
+    sec = (sec < 10 ? '0' : '') + sec;
+
+    let year = date.getFullYear();
+
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? '0' : '') + month;
+
+    let day = date.getDate();
+    day = (day < 10 ? '0' : '') + day;
+
+    return `${year}-${month}-${day} ${hour}:${min}:${sec}`;
+})();
+
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production' ? '/guess-them-all/' : '/',
 
@@ -21,6 +44,12 @@ module.exports = {
             directoryIndex: 'index.html',
             clientsClaim: true,
             skipWaiting: true,
+        },
+    },
+
+    pluginOptions: {
+        ghPages: {
+            message: 'Automatic build ' + currentDateTime,
         },
     },
 };
